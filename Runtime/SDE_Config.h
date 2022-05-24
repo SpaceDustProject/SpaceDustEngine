bc45d0e17626d8fb1820ef92b70921e08be1e254
hszOfficial-16 <1760432001@qq.com>
2022-05-24 19:08:51 +0800

    Alpha-0.0.2

--- Runtime/SDE_Config.h
+++ Runtime/SDE_Config.h
@@ -1,16 +1,11 @@
 #ifndef _SDE_CONFIG_H_
 #define _SDE_CONFIG_H_
 
-#define SDE_MAJOR_VERSION	"0"
-#define SDE_MINOR_VERSION	"0"
-#define SDE_PATCH_LEVEL		"2"
+#define SDE_MAJOR_VERSION	0
+#define SDE_MINOR_VERSION	0
+#define SDE_PATCH_LEVEL		2
 
-#define SDE_VERSION	"SDE"			\
-		SDE_MAJOR_VERSION "."		\
-		SDE_MINOR_VERSION "."		\
-		SDE_PATCH_LEVEL
-
-#define SDE_VERSION_NUM				\
+#define SDE_VERSION					\
 		SDE_MAJOR_VERSION * 1000 +	\
 		SDE_MINOR_VERSION * 100 +	\
 		SDE_PATCH_LEVEL
